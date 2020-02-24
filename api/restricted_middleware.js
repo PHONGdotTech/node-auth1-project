aa2966d19acd2e82d2462b436250704664c36436
Phong Yang <phongyang88@hotmail.com>
2020-02-24 17:30:26 -0600

    added unique constraint to username in table creation migration. added if-else statements to verify username and password are in header to allow using the users router

--- api/restricted_middleware.js
+++ api/restricted_middleware.js
@@ -2,16 +2,20 @@ const bcrypt = require("bcryptjs")
 const Users = require("./routers/users/userModel")
 
 module.exports = (req,res,next) => {
-    Users.findBy({username: req.headers.username})
+    if (req.headers.username && req.headers.password) {
+        Users.findBy({username: req.headers.username})
         .then(user => {
             if (user && bcrypt.compareSync(req.headers.password, user.password)){
                 next()
             } else {
                 res.status(401).json({message: "Invalid Credentials."})
             }
-            
         })
         .catch(err => {
             res.status(500).json(err)
         })
+    } else {
+        res.status(401).json({message: "Missing credentials in header."})
+    }
+    
 }
